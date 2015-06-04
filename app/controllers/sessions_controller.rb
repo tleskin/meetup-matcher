57219d686a88dd061f2a39bf1bf401594b61568d
Michelle Golle <michelleg54@gmail.com>
2015-06-04 15:46:26 -0600

    WIP test for logging in with oauth

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -4,7 +4,7 @@ class SessionsController < ApplicationController
   def create
     user = User.find_or_create_from_auth(auth)
     if user
-      session[:user_id] = user.id 
+      session[:user_id] = user.id
       redirect_to user_path(user)
     else
       redirect_to root_path
--- spec/models/user_spec.rb
+++ spec/models/user_spec.rb
@@ -1,8 +1,8 @@
 require 'rails_helper'
 
 RSpec.describe User, type: :model do
-  xcontext "#find_or_create_from_auth(auth)" do 
-    it "will find or create authenticated user" do 
+  xcontext "#find_or_create_from_auth(auth)" do
+    it "will find or create authenticated user" do
 
       auth = User.create({provider: "Twitter", uid: "120938458475", nickname: "georgec"})
       user = User.find_or_create_from_auth(auth)
